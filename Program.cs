a61d41d295bf60b43cbddac5102c3c9bedd6e14a
DallinO <dallinmolson@gmail.com>
2022-04-28 18:51:21 -0600

    Update

--- Program.cs
+++ Program.cs
@@ -7,20 +7,20 @@ namespace CSE210_01
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Tic-Tac-Toe");
-            List<int> positionValues = new List<int>();
+            List<string> positionValues = new List<string>();
             int index = 1;
             while (index < 10)
             {
-                positionValues.Add(index);
-                ++index;
+                positionValues.Add(index.ToString());
+                index++;
             }
 
             for (index = 0; index < positionValues.Count; index++)
             {
                 Console.WriteLine(positionValues[index]);
             }
-
-
+            
+            Console.WriteLine($"\t{positionValues[0]} | {positionValues[1]} | {positionValues[2]}");
 
         }   
     }
